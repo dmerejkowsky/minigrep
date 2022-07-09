7e47121cc066c48010d854e6e1af3f2712865fc5
micwoj92 <45581170+micwoj92@users.noreply.github.com>
2022-07-09 20:09:32 +0000

    Fix clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ impl<'a> Config<'a> {
         let query = &args[1];
         let filename = &args[2];
 
-        let case_sensitive = !env::var("MINIGREP_CASE_INSENSITIVE").is_ok();
+        let case_sensitive = env::var("MINIGREP_CASE_INSENSITIVE").is_err();
         Ok(Config {
             query,
             filename,
@@ -28,9 +28,9 @@ impl<'a> Config<'a> {
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     let lines = if config.case_sensitive {
-        search(&contents, &config.query)
+        search(&contents, config.query)
     } else {
-        search_insensitive(&contents, &config.query)
+        search_insensitive(&contents, config.query)
     };
     for line in lines {
         println!("{}", line);
